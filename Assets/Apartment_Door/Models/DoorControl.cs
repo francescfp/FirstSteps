55b4b0cd976c23aca63a4a7ebecb347d167e2458
Francesc <fraferp8@upv.edu.es>
2021-04-16 18:37:16 +0200

    Project 3 changes

---
+++ Assets/Apartment_Door/Models/DoorControl.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DoorControl : MonoBehaviour
+{
+    private Animator _anim;
+
+    private BoxCollider _box;
+
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        _anim = GetComponent<Animator>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        OnTriggerEnter(_box);
+        
+    }
+
+    private void OnTriggerEnter(Collider other)
+    {
+        if (other.name == "Sphere")
+        {
+            _anim.SetTrigger("touched");
+;
+        }
+    }
+}
