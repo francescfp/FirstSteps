75910884610e8175dd1d3e7c48a4ec06afc792cf
Francesc <fraferp8@upv.edu.es>
2021-04-24 23:09:36 +0200

    Lesson 10

---
+++ Assets/Scripts/movement.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class movement : MonoBehaviour
+{
+    public Animator animator;
+    
+    // Start is called before the first frame update
+    void Start()
+    {
+        animator = GetComponent<Animator>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        animator.SetFloat("Vertical", 0);
+        animator.SetFloat("Horizontal", 0);
+
+        if (Input.GetKey(KeyCode.I)) { animator.SetFloat("Vertical", 1); }
+        if (Input.GetKey(KeyCode.J)) { animator.SetFloat("Horizontal", -1); }
+        if (Input.GetKey(KeyCode.K)) { animator.SetFloat("Vertical", -1); }
+        if (Input.GetKey(KeyCode.L)) { animator.SetFloat("Horizontal", 1); }
+    }
+}
